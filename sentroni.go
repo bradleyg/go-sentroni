5f48143cb422cd62aedb287098180f75cd86ee2f
Bradley Griffiths <bradley.griffiths@gmail.com>
2014-11-03 18:24:25 +0000

    Docs.

--- sentroni.go
+++ sentroni.go
@@ -1,3 +1,10 @@
+// Recovery is a Negroni middleware that recovers from any panics, writes a
+// 500 and passes the error to Sentry.
+//
+// Usage:
+//   n := negroni.New()
+//   n.Use(sentroni.NewRecovery(sentryDsn))
+//   n.Run()
 package sentroni
 
 import (
@@ -10,15 +17,13 @@ import (
 	"github.com/getsentry/raven-go"
 )
 
-// Recovery is a Negroni middleware that recovers from any panics, writes a
-// 500 and passes the error to Sentry.
-type Recovery struct {
+type recovery struct {
 	Logger *log.Logger
 	Client *raven.Client
 }
 
-// NewRecovery returns a new instance of Recovery. Pass your Sentry dsn as an argument.
-func NewRecovery(dsn string) *Recovery {
+// NewRecovery returns a *http.Handler interface. Pass your Sentry dsn as an argument.
+func NewRecovery(dsn string) *recovery {
 	logger := log.New(os.Stdout, "[sentroni] ", 0)
 
 	client, err := raven.NewClient(dsn, nil)
@@ -26,13 +31,13 @@ func NewRecovery(dsn string) *Recovery {
 		logger.Fatal("FATAL: ", err)
 	}
 
-	return &Recovery{
+	return &recovery{
 		Logger: logger,
 		Client: client,
 	}
 }
 
-func (rec *Recovery) ServeHTTP(rw http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+func (rec *recovery) ServeHTTP(rw http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
 	defer func() {
 		if err := recover(); err != nil {
 			rec.Logger.Printf("PANIC: %s\n%s", err, debug.Stack())
